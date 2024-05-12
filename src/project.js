bed54ded8327ac3c92aca0b3b525a400f8b5ab3e
NadifRahman <nadif.rah@gmail.com>
2024-05-12 15:23:22 -0400

    Create project module

---
+++ src/project.js
@@ -0,0 +1,34 @@
+export default class Project {
+    title;//String 
+    todoList; //array of Todo objects
+
+    constructor(title) {
+        this.title = title; //sets title of project 
+        this.todoList = []; //initalize as empty array
+    }
+
+    /**
+     *  Adds todo object to the instance's todoList array
+     * @param {A Todo object} todoObject 
+     */
+    addTodo(todoObject) {
+        this.todoList.push(todoObject); 
+    }
+
+    /**
+     * deletes a todo object in the instance's todolist given an index
+     * @param {integer number > -1 and < todoList.length} index 
+     */
+
+    deleteTodo(index) {
+        this.todoList.splice(index, 1);
+    }
+    /**
+     * returns a todo object from the instance's todolist given an index
+     * @param {integer number > -1 and < todoList.length} index 
+     */
+    getTodo(index){
+        return this.todoList[index];
+
+    }
+}
